d48afcdbfa108c0b61f1a6890210e30cc0e3789d
hungry-developer <34088643+hungry-developer@users.noreply.github.com>
2019-07-11 11:18:12 +0600

    dot-net-session

---
+++ Session Demo/Controllers/SessionDemoController.cs
@@ -0,0 +1,41 @@
+﻿using Session_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Session_Demo.Controllers
+{
+    public class SessionDemoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Demo()
+        {
+            List<Employee> employees1 = Employees();
+            Session["MyDatatble"] = employees1;
+
+
+            ViewBag.Emp = (List<Employee>)Session["MyDatatble"];
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Demo(string tb)
+        {
+            //List<Employee> employees1 = Employees();
+            ViewBag.Emp = (List<Employee>)Session["MyDatatble"];
+
+            ViewBag.Message = "Employee Saved";
+            return View();
+        }
+        public List<Employee> Employees()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee(){Name = "Rafiq",Des = "IT"},
+                new Employee(){Name = "jaman",Des = "IT"},
+            };
+            return employees;
+        }
+    }
+}
---
+++ Session Demo/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Session_Demo.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Des { get; set; }
+    }
+}
